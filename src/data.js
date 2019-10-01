9356bacfadce89a87ec0251f04f2514484b7411f
macarenasaez <makadaira@gmail.com>
2019-09-30 23:01:31 -0300

    agrego cambios

---
+++ src/data.js
@@ -0,0 +1,33 @@
+/* Manejo de data */
+
+// esta es una función de ejemplo
+// puedes ver como agregamos la función a nuestro objeto global window
+
+
+
+function filterData (data,species){
+  let filterResult = data.filter(element =>{
+    return element.species.includes(species)
+  })
+  return filterResult
+}
+window.filterData = filterData;
+
+//funcion para ordenar 
+const containerRoot = document.getElementById("root");
+selectionOrder = document.getElementById("name");
+const sortBy = (rickData,seletSort)=>{
+  let  orderRick  = rickData;
+  if (seletSort === "a-z"){
+    orderRick = data.sort((a,b)=> a.name.localeCompare(b.name));
+  }
+  if (seletSort === "z-a"){
+    orderRick = data.sort((a, b) => a.name.localeCompare(b.name)).reverse();
+  }
+  return (orderRick);
+}
+
+sortBy();
+window.sortBy = sortBy;
+
+
---
+++ src/main.js
@@ -0,0 +1,79 @@
+
+const rickData = window.RICKANDMORTY.results;
+
+let container = document.getElementById("root");
+document.getElementById("rickanDmorty").addEventListener("change",() => {
+
+  let speciesFilter = document.getElementById ("rickanDmorty").value;
+const results = filterData(rickData,speciesFilter);
+
+//console.log(results);
+
+const createCards = (results) => {
+  let characters = "";
+  results.forEach(element => {
+    const cards = `
+  
+    <div class="flip-card">
+  <div class="flip-card-inner">
+  
+   <div class="flip-card-front">
+     <img id="img-card" src=${element.image} alt="Imagen">
+     <h4>${element.name}</h4> 
+    </div>
+  
+    <div class="flip-card-back">
+  
+    <p>${element.status}</p>
+    <p>${element.species}</p>
+    <p>${element.type}</p>
+    <p>${element.gender}<p>
+  </div>
+  
+  </div>
+  </div>`
+  characters += cards;
+
+  });
+  container.innerHTML= characters;
+
+  }
+  createCards(results);
+
+});
+
+
+
+
+//ordenar
+
+const rikaOrder = getElementById ("order");
+
+ rikaOrder.addEventListener("changes",()=>{
+   let sortOrder = rikaOrder.value;
+   selectionOrder =sortrick(rickData,"name",sortOrder);
+   container.innerHTML=""; 
+   selectionOrder.forEach(element=>{
+     container.innerHTML+=  `
+     <div class="flip-card">
+<div class="flip-card-inner">
+  <div class="flip-card-front">
+    <img id="img-card" src=${element.img} alt="Imagen">
+    <h4>${element.name}</h4> 
+
+    <div class="flip-card-back">
+  
+    <p>${element.status}</p>
+    <p>${element.species}</p>
+    <p>${element.type}</p>
+    <p>${element.gender}<p>
+  </div>
+  
+  </div>
+  </div>`
+   })
+ });
+
+
+
+   
